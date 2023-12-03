d93d51af5cc151575f068e7889f0ca101c2302f8
watackeech <devkeech@gmail.com>
2023-12-03 18:34:16 +0900

    built for some added pages

---
+++ .output/public/_nuxt/index.afdedd46.js
@@ -0,0 +1,1 @@
+import{_ as e,v as o,t,x as c}from"./entry.4ebb5101.js";const n={},s=c("h1",null,"Hello World",-1),r=[s];function _(a,l){return t(),o("div",null,r)}const i=e(n,[["render",_]]);export{i as default};
