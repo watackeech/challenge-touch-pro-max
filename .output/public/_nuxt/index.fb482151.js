0d8728bd61780978108b8ee2316bc204ba4191e0
watackeech <devkeech@gmail.com>
2023-12-03 19:05:59 +0900

    updated pnpm

--- .output/public/_nuxt/index.fb482151.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,v as t,t as s,x as n}from"./entry.493008a5.js";const o={},c=n("h1",null,"Want to start lesson?",-1),a=[c];function r(_,l){return s(),t("div",null,a)}const i=e(o,[["render",r]]);export{i as default};
