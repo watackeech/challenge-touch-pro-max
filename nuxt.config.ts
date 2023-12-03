f9e79e915226e6e9a6b83e6b57dc377a3313fcf8
watackeech <devkeech@gmail.com>
2023-12-03 23:40:32 +0900

    pwa化

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,43 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+import { defineNuxtConfig } from 'nuxt/config'
+
 export default defineNuxtConfig({
+  // app: {
+  //   link: [
+  //     { rel: "manifest", href: "/manifest.webmanifest" },
+  //   ]
+  // },
   devtools: { enabled: true },
-  modules: ['@nuxt/image']
+  modules: ['@nuxt/image','@vite-pwa/nuxt'],
+  pwa: {
+    registerType: "autoUpdate",
+    includeAssets: ["favicon.ico"],
+    client: {
+      installPrompt: true,
+    },
+    manifest: {
+      name: 'name',
+      description: "description",
+      theme_color: "#ffffff",
+      lang: "ja",
+      short_name: "short_name",
+      start_url: "/",
+      display: "standalone",
+      background_color: "#ffffff",
+      icons: [
+        {
+          "src": "favicon.ico",
+          "sizes": "72x72 96x96 128x128 256x256"
+        },
+      ]
+    },
+    workbox: {
+      navigateFallback: null
+    },
+    devOptions: {
+      enabled: true,
+      type: "module"
+    },
+  },
 })
